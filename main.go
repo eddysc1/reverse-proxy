d9e545ea512cd9db861dc0c72eeb1109c8a4c57b
Edvardas Gaidelis <edvardassc@gmail.com>
2022-02-16 16:39:34 +0200

    create reverse proxy

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
+func NewProxy(targetHost string) *httputil.ReverseProxy {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		panic(err)
+	}
+
+	return httputil.NewSingleHostReverseProxy(url)
+}
+
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
 func main() {
-	fmt.Println("simple reverse proxy")
+	proxy := NewProxy("https://fplnow.com")
+
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	http.ListenAndServe(":8080", nil)
 }
